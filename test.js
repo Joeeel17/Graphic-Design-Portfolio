26dd42a304f1fe79f38caf73102d54d7d796b2bc
Joeeel17 <joelbalang34@gmail.com>
2021-07-05 11:33:01 +0800

    Initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+AOS.init({
+        duration: 1800,
+    });
+
+$("#myCarousel").carousel();
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
